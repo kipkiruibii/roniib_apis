52d57618672be56c4d809936adbec5934339bf0b
kipkiruibii <biihezron1@gmail.com>
2023-09-06 14:25:09 +0300

    4 commit

--- roniib_api/admin.py
+++ roniib_api/admin.py
@@ -5,3 +5,4 @@ from .models import *
 # Register your models here.
 admin.site.register(ApiDocumentation)
 admin.site.register(ApiEndpoints)
+admin.site.register(APICategories)
--- roniib_api/models.py
+++ roniib_api/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class APICategories(models.Model):
+    category=models.CharField(max_length=255)
+    category_short=models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.category)
 
 class ApiDocumentation(models.Model):
+    api_category=models.ForeignKey(APICategories,on_delete=models.CASCADE,default='')
     api_name=models.CharField(max_length=255)
-    short_name=models.CharField(max_length=255,default='f')
+    short_name=models.CharField(max_length=255,default='s_name')
     api_intro=models.TextField(default='intro')
+    api_brief_description=models.TextField(default='intro')
+    api_image=models.CharField(max_length=255,default='url_img')
+    api_subscribers=models.IntegerField(default=0)
     def __str__(self):
         return str(self.api_name)
-
 
 class ApiEndpoints(models.Model):
     api_name=models.ForeignKey(ApiDocumentation,on_delete=models.CASCADE)
     endpoint_name=models.CharField(max_length=255)
     endpoint_url=models.CharField(max_length=255)
-    endpoint_desc=models.TextField(default='api desc')
-
+    endpoint_desc=models.TextField(default='api description')
     def __str__(self):
         return str(self.api_name)
